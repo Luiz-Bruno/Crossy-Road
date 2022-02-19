7c09138bde77c766450607da9899cce861342be5
Luiz Bruno <luiz_seya@hotmail.com>
2022-02-19 18:29:37 -0300

    Movimentação do player

---
+++ Crossy Road/Assets/Player.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float jumpForce = 112f;
+    public float groundCheckDistance = 0.3f;
+    private bool isGrounded = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast((transform.position + Vector3.up * 0.1f), Vector3.down, groundCheckDistance))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                AdjustPositionAndRotation(new Vector3(0, 0, 0));
+                rb.AddForce(new Vector3(0, jumpForce, jumpForce));
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                AdjustPositionAndRotation(new Vector3(0, 180, 0));
+                rb.AddForce(new Vector3(0, jumpForce, -jumpForce));
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                AdjustPositionAndRotation(new Vector3(0, -90, 0));
+                rb.AddForce(new Vector3(-jumpForce, jumpForce, 0));
+            }
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                AdjustPositionAndRotation(new Vector3(0, 90, 0));
+                rb.AddForce(new Vector3(jumpForce, jumpForce, 0));
+            }
+        }
+    }
+
+    void AdjustPositionAndRotation(Vector3 newRotation)
+    {
+        rb.velocity = Vector3.zero;
+        transform.eulerAngles = newRotation;
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Round(transform.position.z));
+    }
+}
